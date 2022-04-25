4ade94f93195125dcfe6ce6c68407fb5206dfab9
talha <contact@talhaaydin.com>
2022-04-26 00:24:24 +0300

    Converting syntax to more rust like way

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::io;
+use std::ffi::c_void;
+use std::{cmp, io, mem};
 use std::path::Path;
 use std::process::{Command, exit, ExitStatus};
-
-use windows_sys::{
-    Win32::Foundation::*, Win32::System::Threading::*, Win32::UI::WindowsAndMessaging::*
+use std::ptr;
+use windows::{
+    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+    Win32::UI::WindowsAndMessaging::*
 };
+use windows::Win32::Security::SECURITY_ATTRIBUTES;
+use std::time::Duration;
+use std::thread::sleep;
+
 // Trait std::os::windows::process::ExitStatusExt
 // https://grep.app/search?q=std%3A%3Aos%3A%3Awindows%3A%3Aprocess%3A%3AExitStatusExt
 
@@ -35,13 +41,13 @@ fn call_messagebox() {
 
 fn main() -> io::Result<()> {
     // run calc.exe
-    let mut calc_exe = b"C:\\Windows\\System32\\calc.exe\0";
+    let mut calc_exe = PCSTR("C:\\Windows\\System32\\calc.exe\0".as_ptr());
     let creation_fags = DEBUG_PROCESS; // DEBUG_FLAG
     let mut startupinfo = STARTUPINFOA {
         cb: std::mem::size_of::<STARTUPINFOA>() as u32,
-        lpReserved: std::ptr::null_mut(),
-        lpDesktop: std::ptr::null_mut(),
-        lpTitle: std::ptr::null_mut(),
+        lpReserved: PSTR(ptr::null_mut()),
+        lpDesktop: PSTR(ptr::null_mut()),
+        lpTitle: PSTR(ptr::null_mut()),
         dwX: 0,
         dwY: 0,
         dwXSize: 0,
@@ -49,39 +55,39 @@ fn main() -> io::Result<()> {
         dwXCountChars: 0,
         dwYCountChars: 0,
         dwFillAttribute: 0,
-        dwFlags: 0x1,
+        dwFlags: STARTUPINFOW_FLAGS(0x1),
         wShowWindow: 0x0,
         cbReserved2: 0,
-        lpReserved2: std::ptr::null_mut(),
-        hStdInput: 0,
-        hStdOutput: 0,
-        hStdError: 0,
+        lpReserved2: ptr::null_mut(),
+        hStdInput: HANDLE(0),
+        hStdOutput: HANDLE(0),
+        hStdError: HANDLE(0),
     };
 
     let mut process_information = PROCESS_INFORMATION {
-        hProcess: isize,
-        hThread: NULL as HANDLE,
+        hProcess: HANDLE(0),
+        hThread: HANDLE(0),
         dwProcessId: 0,
         dwThreadId: 0,
     };
     unsafe {
         let process = CreateProcessA(calc_exe,
-                                     NULL as LPSTR,
-                                     std::ptr::null_mut(),
-                                     std::ptr::null_mut(),
-                                     0,
+                                     PSTR(ptr::null_mut()),
+                                     ptr::null(),
+                                     ptr::null(),
+                                     BOOL(0),
                                      creation_fags,
-                                     NULL,
-                                     NULL as LPCSTR,
+                                     0 as *mut c_void,
+                                     PCSTR(ptr::null_mut()),
                                      &mut startupinfo,
                                      &mut process_information,
         );
-        if process != 0 {
-            print!("[*] We have successfully launched the process!");
-            print!("[*] The Process ID of running process is: {}", process_information.dwProcessId);
-            WaitForSingleObject(process_information.hProcess, INFINITE);
+        if process.0 != 0 {
+            print!("[*] We have successfully launched the process!\n");
+            print!("[*] The Process ID of running process is: {}\n", process_information.dwProcessId);
+            //sleep(Duration::from_secs(10));
+            WaitForSingleObject(process_information.hProcess, 0xFFFFFFFF);
         }
     }
-
     Ok(())
 }
